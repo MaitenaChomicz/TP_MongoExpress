34a8f741a1faf3ca4d280e0b0ab30ee0b4292852
Maitena Chomicz <maitenac2003@gmail.com>
2025-06-06 20:33:13 -0300

    Conexión a la base de datos

---
+++ src/config/mongo.ts
@@ -0,0 +1,14 @@
+import { connect } from "mongoose";
+process.loadEnvFile()
+
+const URI_DB = process.env.URI_DB || ""
+
+const connectMongoDB = async () => {
+  try {
+    await connect(URI_DB)
+    console.log("Base de datos conectada correctamente")
+  } catch (error) {
+    console.log("No es posible conectarse a la base de datos")
+  }
+}
+export { connectMongoDB }
